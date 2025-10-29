f2a7962fc7a0ab8b81db76c4df0a8658f1d79598
Anoop Rajoriya <anooprajoriya9827@gmail.com>
2025-10-29 13:51:18 +0530

    add clerk auth

--- proxy.ts
+++ proxy.ts
@@ -5,23 +5,20 @@ import { NextResponse } from "next/server";
 // 1. Routes that require authenticaitn (User only)
 const isProtectedRoute = createRouteMatcher([
   "/dashboard",
+  "/social-share",
+  "/video-upload",
   "/api/protected/(.*)",
 ]);
 // 2. Routes that should not be accessible when authenticated (Public only)
 const isPublicOnlyRoute = createRouteMatcher([
-  "/auth/sign-in",
-  "/auth/sign-up",
+  "/auth/sign-in/(.*)",
+  "/auth/sign-up/(.*)",
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
   const { userId } = await auth();
-<<<<<<< HEAD
-=======
-  const currentUrl = new URL(req.url);
->>>>>>> af728d70249d3bc8b6734eb4b10eb279c8f91120
 
   // 1. Protect user only routes
-
   if (!userId && isProtectedRoute(req)) {
     return NextResponse.redirect(new URL("/auth/sign-in", req.url));
   }
