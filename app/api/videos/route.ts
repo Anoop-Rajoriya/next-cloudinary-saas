8652e47a7af024e30d74faf475d3280b2ce0a0f6
Anoop Rajoriya <anooprajoriya9827@gmail.com>
2025-10-30 17:26:41 +0530

    add prisma, neon db setup

--- app/api/videos/route.ts
+++ app/api/videos/route.ts
@@ -1,5 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@/generated/prisma/client";
+
+const prisma = new PrismaClient();
 
 export async function GET(req: NextRequest) {
-  return NextResponse.json({ res: "ok" });
+  try {
+    const videos = await prisma.video.findMany({
+      orderBy: { createdAt: "desc" },
+    });
+
+    return NextResponse.json(videos);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to fetch videos" },
+      { status: 500 }
+    );
+  } finally {
+    prisma.$disconnect();
+  }
 }
